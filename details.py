589f8e1f3bef0a4a289655e6bd023807d8c73d26
tobbootcamp <tobiaschauke4@gmail.com>
2025-03-30 21:31:42 +0200

    Update details.py

--- details.py
+++ details.py
@@ -1,9 +1,12 @@
+*****PSUEDO COMMENTS*********
 
 # open a python file in a folder called L1T02.
 # open a new file and call it,details.py.
 # enter the user`s name,age,house number and street number.
 # print all the user`s detailed information using the print function.
 
+****** INPUT THE VARIABLES **********
+  
 name=input('name of the user: ')
 age=input('age of the user: ')
 house_number=input('house number of the user: ')
