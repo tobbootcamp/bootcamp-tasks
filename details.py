500a3812a194e525496d0a290a99a6f011de5eca
tobbootcamp <tobiaschauke4@gmail.com>
2025-03-30 22:18:01 +0200

    Update details.py

--- details.py
+++ details.py
@@ -1,14 +1,17 @@
-*****PSUEDO COMMENTS*********
+***** PSUEDO COMMENTS *********
 
 # open a python file in a folder called L1T02.
 # open a new file and call it,details.py.
 # enter the user`s name,age,house number and street number.
 # print all the user`s detailed information using the print function.
 
-****** INPUT THE VARIABLES **********
+****** USING THE INPUT COMMAND TO GET THE INFORMATION OF THE USER **********
   
 name=input('name of the user: ')
 age=input('age of the user: ')
 house_number=input('house number of the user: ')
 street_name=input('street name of the user: ')
+
+***** PRINT THE SENTENCE CONTAINING THE DETAILS OF THE USER ******** 
+
 print(f"This is {name}. He is {age} and lives at house number {house_number} {street_name}. ")
